8bf3443d9c520a1fc6d7b878ce66b0b0084909b1
Apoorva Avadhana <apoorva.a1998@gmail.com>
2018-06-24 00:00:35 +0530

    Binomial Expansion

---
+++ 0086-BinomialExpansion/binomialexpansion.py
@@ -0,0 +1,14 @@
+x=int(input("enter binomial expansion number"))
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n * factorial(n-1)
+#x=str(factorial(100))
+
+def binomial_expansion(X):
+    for i in range(X):
+        print(factorial(X)/(factorial(X-i)*factorial(i)),"x^",X-i,"y^",i," +",end='')
+
+binomial_expansion(x)
